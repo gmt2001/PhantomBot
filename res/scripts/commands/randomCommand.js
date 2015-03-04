8c8297d95cdaf69db4aa5766f1aaed981f46e8af
phantomindex <thephantomindex@gmail.com>
2015-03-03 23:25:56 -0500

    Fixed randomCommand.js and added (points) to addcom

--- res/scripts/commands/randomCommand.js
+++ res/scripts/commands/randomCommand.js
@@ -124,64 +124,64 @@ $.on('command', function(event) {
 
     if (messageCommand) {
         for (var i = 0; i < args.length; i++) {
-            while (messageCommand.contains('<' + (i + 1) + '>')) {
-                messageCommand = messageCommand.replace('<' + (i + 1) + '>', args[i]);
+            while (messageCommand.contains('(' + (i + 1) + ')')) {
+                messageCommand = messageCommand.replace('(' + (i + 1) + ')', args[i]);
             }
         }
 
-        while (messageCommand.contains('<sender>')) {
-            messageCommand = messageCommand.replace('<sender>', $.username.resolve(sender));
+        while (messageCommand.contains('(sender)')) {
+            messageCommand = messageCommand.replace('(sender)', $.username.resolve(sender));
         }
 
-        while (messageCommand.contains('<user>')) {
-            messageCommand = messageCommand.replace('<user>', $.username.resolve(sender));
+        while (messageCommand.contains('(user)')) {
+            messageCommand = messageCommand.replace('(user)', $.username.resolve(sender));
         }
 
-        while (messageCommand.indexOf('<count>') != -1) {
-            messageCommand = messageCommand.replace('<count>', $.inidb.get('counter', command));
+        while (messageCommand.indexOf('(count)') != -1) {
+            messageCommand = messageCommand.replace('(count)', $.inidb.get('counter', command));
         }
 
-        while (messageCommand.indexOf('<random>') != -1) {
-            messageCommand = messageCommand.replace('<random>', $.username.resolve(randomPerson));
+        while (messageCommand.indexOf('(random)') != -1) {
+            messageCommand = messageCommand.replace('(random)', $.username.resolve(randomPerson));
         }
-        while (messageCommand.indexOf('<#>') != -1) {
-            messageCommand = messageCommand.replace('<#>', $.username.resolve(randomNum));
+        while (messageCommand.indexOf('(#)') != -1) {
+            messageCommand = messageCommand.replace('(#)', $.username.resolve(randomNum));
         }
 
         $.say(messageCommand);
     }
 });
     var ar = new Array(0);
-        ar.push("<sender> was welcomed to the JAM so now it's time for <sender> to get SLAMMED!");
-        ar.push("<sender> was thrown into a large pit of PJSalt.");
-        ar.push("Oh my, <sender> you make me just want to *SLURP SLURP SLURP*");
-        ar.push("<sender> was thrown into a pit of tentacle pleasure.");
-		ar.push("<target> was thrown into a pit of genetically modified super lions.");
-		ar.push("<target> was thrown into a pit of explosive prinnys.");
-        ar.push("I want you <sender>.");
-        ar.push("/me threw <sender> into a pit of PJSalt!" );
-        ar.push("Dango, dango, dango, dango, big <sender> family~\u266B");
-        ar.push("What are we gonna do on the bed <sender>?");
+        ar.push("(sender) was welcomed to the JAM so now it's time for (sender) to get SLAMMED!");
+        ar.push("(sender) was thrown into a large pit of PJSalt.");
+        ar.push("Oh my, (sender) you make me just want to *SLURP SLURP SLURP*");
+        ar.push("(sender) was thrown into a pit of tentacle pleasure.");
+		ar.push("(target) was thrown into a pit of genetically modified super lions.");
+		ar.push("(target) was thrown into a pit of explosive prinnys.");
+        ar.push("I want you (sender).");
+        ar.push("/me threw (sender) into a pit of PJSalt!" );
+        ar.push("Dango, dango, dango, dango, big (sender) family~\u266B");
+        ar.push("What are we gonna do on the bed (sender)?");
         ar.push("Your mother was a hamster and your father smelt of elderberries!");
-        ar.push("I would rather not acknowledge that <sender> typed '!random'");
-        ar.push("<sender> was standing in the park wondering why a Frisbee got bigger as it got closer. Then it hit 'em.");
-        ar.push("When life gives you lemons <sender>, make orange juice and leave the world wondering how the hell you did it.");
-        ar.push("<sender> was violated by a tentacle monster on stream.");
-        ar.push("/me licked <sender>. . .");
-        ar.push("THIS JUST IN! <sender>'s waifu is actually in a ahegao doujin!");
-        ar.push("This is the first time <sender> has noticed the scent of a woman.");
-        ar.push("<sender> you're a beautiful strong black woman.");
+        ar.push("I would rather not acknowledge that (sender) typed '!random'");
+        ar.push("(sender) was standing in the park wondering why a Frisbee got bigger as it got closer. Then it hit 'em.");
+        ar.push("When life gives you lemons (sender), make orange juice and leave the world wondering how the hell you did it.");
+        ar.push("(sender) was violated by a tentacle monster on stream.");
+        ar.push("/me licked (sender). . .");
+        ar.push("THIS JUST IN! (sender)'s waifu is actually in a ahegao doujin!");
+        ar.push("This is the first time (sender) has noticed the scent of a woman.");
+        ar.push("(sender) you're a beautiful strong black woman.");
         ar.push("/me licking intensifies!");
-        ar.push("༼ つ ◕_◕ ༽=ε̵͇̿̿'̿'̿ ̿ ̿̿  " + " I has a gun, Gimme yo money <sender>!");
-        ar.push("I hope <sender>-senpai notices me..");
-        ar.push("<sender> is so awesome because <sender> can trip over flat surfaces, fall up the stair and also fall up!!");
-        ar.push("<sender> looks good enough to dress like the grim reaper and go to a retirement home and tap on the windows!");
+        ar.push("༼ つ ◕_◕ ༽=ε̵͇̿̿'̿'̿ ̿ ̿̿  " + " I has a gun, Gimme yo money (sender)!");
+        ar.push("I hope (sender)-senpai notices me..");
+        ar.push("(sender) is so awesome because (sender) can trip over flat surfaces, fall up the stair and also fall up!!");
+        ar.push("(sender) looks good enough to dress like the grim reaper and go to a retirement home and tap on the windows!");
         ar.push("Ok everyone shhhhh. The rice crispies are telling me what to do next.");
-        ar.push("<sender> made eye contact with <target> while eating a banana!");
-        ar.push("It's weird, <sender> just doesn't have enough sax appeal.");
-        ar.push("<target> mysteriously died.");
-		ar.push("<target> stopped existing.");
-	    ar.push("All 7 of the Dragon Balls have been gathered! Now to wish <target> to become a sloth!");
+        ar.push("(sender) made eye contact with (target) while eating a banana!");
+        ar.push("It's weird, (sender) just doesn't have enough sax appeal.");
+        ar.push("(target) mysteriously died.");
+		ar.push("(target) stopped existing.");
+	    ar.push("All 7 of the Dragon Balls have been gathered! Now to wish (target) to become a sloth!");
 		ar.push("The ocean is so salty because everyone pees in it.");
 		ar.push("Real men know when to run like a little bitch");
 		ar.push("If I get reincarnated…. I wanna become a clam.");
@@ -190,9 +190,9 @@ $.on('command', function(event) {
 		ar.push("And while you’re waiting for us to kill you, we highly recommend p*ssing yourself, Followed by a course of praying to your impudent god. And cowarding in the corner and begging, always good. But if you act now, theres still time for an old-fashioned Suicide!! Thank you London! We love you! goodnight!!");
 		ar.push("If you die, don’t come crying to me about it.");
 		ar.push("From this point on, all you opinions will be rejected!");
-		ar.push("Kakrot! Is That A Vegetable?! I Hate Vegetables.");
+		ar.push("Kakarot! Is That A Vegetable?! I Hate Vegetables.");
 		ar.push("You have no effect on me because you are flast chested!");
-		ar.push("I see. <sender> is the type of person who is thinking positively to the extent of being stupid.");
+		ar.push("I see. (sender) is the type of person who is thinking positively to the extent of being stupid.");
 		ar.push("I know as much of games as hugs and puppies, and care for them even less.");
 		ar.push("I’m not just a pervert…I’M A SUPER PERVERT");
 		ar.push("When you meet with scary people, you must always protect your wallet and asshole!");
@@ -207,12 +207,12 @@ $.on('command', function(event) {
 		ar.push("Do evil, get caught, then claim demons were brainwashing you. A common way that human politicians evade responsibility.");
 		ar.push("I have one last request. Please slap me in the face with that wad of cash.");
 		ar.push("Why do people have to work? I just want to eat and sleep. I should have been born as a panda at the zoo.");
-		ar.push("Right, I have something I have to apologize to <target> for. Sometimes, when we were playing, you’d suddenly start crying, and then you’d run home. You probably thought you’d fooled us, but everyone knew… that you had pooped your pants! Sorry.");
+		ar.push("Right, I have something I have to apologize to (target) for. Sometimes, when we were playing, you’d suddenly start crying, and then you’d run home. You probably thought you’d fooled us, but everyone knew… that you had pooped your pants! Sorry.");
 		ar.push("What I want to do most? Let’s see… Just once, I really want to let loose and pee in my pants.");
 		ar.push("Dude! Christmas rocks! We know how to do it right, here! First, we X out the ‘Christ’ part to make it extreme! Then we shop and eat stuff ’til we’re sick! Wanna shovel down some X-Mas cake to get in the spirit?");
 		ar.push("My name is Duck. Yes, like the bird. Take it up with my parents. They never loved me!");
 		ar.push("Boobs that don’t shake, aren’t boobs at all.");
-		ar.push("<sender>, during your last physical it was discovered you have Athletes Foot, and we now believe it is affecting your brain.");
+		ar.push("(sender), during your last physical it was discovered you have Athletes Foot, and we now believe it is affecting your brain.");
 		ar.push("When you have a swimsuit tan. You could jump in the pool naked and no one would know you weren’t wearing anything.");
 
 
