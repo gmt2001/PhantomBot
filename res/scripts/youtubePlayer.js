229ff2d3147c87231cae6265878b6b25f18f8aea
GloriousEggroll <gloriouseggroll@gmail.com>
2015-03-09 17:03:03 -0400

    a bit cleaner youtube url parsing

--- res/scripts/youtubePlayer.js
+++ res/scripts/youtubePlayer.js
@@ -64,7 +64,7 @@ function Song(name) {
 }
 
 function youtubeParser(url){
-    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?^\s]*).*/ ;
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?^\s]*).*/;
     var match = url.match(regExp);
     if (match&&match[7].length==11){
         return match[7];
