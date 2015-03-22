67223a50e0878d27a45fc31f4a43cec271f9ed30
phantomindex <thephantomindex@gmail.com>
2015-03-21 20:24:23 -0400

    Ipdated top10 to have top10time and cleaned some messages up.

--- res/scripts/commands/top10Command.js
+++ res/scripts/commands/top10Command.js
@@ -18,7 +18,7 @@ $.on('command', function(event) {
         var b;
         var k;
         var points;
-        var s = "Top 10 >>> ";
+        var s = "Top 10 " + $.pointsname + ">> ";
         
         for (i = 0; i < keys.length; i++) {
             if (!keys[i].equalsIgnoreCase($.botname) && !keys[i].equalsIgnoreCase($.botowner)) {
@@ -47,6 +47,51 @@ $.on('command', function(event) {
         
         $.say(s);
     }
+    
+    if (command.equalsIgnoreCase("top10time")) {
+        if (!$.moduleEnabled("./systems/timeSystem.js")) {
+            return;
+        }
+        
+        var keys = $.inidb.GetKeyList("time", "");
+        var top10time = new Array(new Array("Nobody", -1), new Array("Nobody", -2), new Array("Nobody", -3),
+            new Array("Nobody", -4), new Array("Nobody", -5), new Array("Nobody", -6),
+            new Array("Nobody", -7), new Array("Nobody", -8), new Array("Nobody", -9), new Array("Nobody", -10));
+        var i;
+        var b;
+        var k;
+        var times;
+        var s = "Top10 Time >>> ";
+        
+        for (i = 0; i < keys.length; i++) {
+            if (!keys[i].equalsIgnoreCase($.botname) && !keys[i].equalsIgnoreCase($.botowner)) {
+                times = parseInt($.inidb.get("time", keys[i]));
+            
+                for (b = 0; b < top10time.length; b++) {
+                    if (top10time[b][1] < times) {
+                        for (k = top10time.length - 2; k >= b; k--) {
+                            top10time[k + 1] = top10time[k];
+                        }
+                    
+                        top10time[b] = new Array($.username.resolve(keys[i]), times);
+                        break;
+                    }
+                }
+            }
+        }
+        
+        for (i = 0; i < top10time.length; i++) {
+            if (i > 0) {
+                s += "  -  ";
+            }
+            
+            s += (i + 1) + ". " + top10time[i][0] + " [approx. " + Math.round(top10time[i][1] / 3600)+ " Hrs]";
+        }
+        
+        $.say(s);
+    }
 });
 
 $.registerChatCommand("./commands/top10Command.js", "top10");
+$.registerChatCommand("./commands/top10Command.js", "top10time");
+;
