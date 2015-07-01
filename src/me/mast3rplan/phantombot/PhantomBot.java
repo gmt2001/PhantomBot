3fce0e31189db03b226187bec44e479acc9fe232
GloriousEggroll <gloriouseggroll@gmail.com>
2015-07-01 00:19:03 -0400

    update build number, remove join/part double post from channeluserscache

--- src/me/mast3rplan/phantombot/PhantomBot.java
+++ src/me/mast3rplan/phantombot/PhantomBot.java
@@ -111,7 +111,7 @@ public class PhantomBot implements Listener
         Thread.setDefaultUncaughtExceptionHandler(com.gmt2001.UncaughtExceptionHandler.instance());
 
         com.gmt2001.Console.out.println();
-        com.gmt2001.Console.out.println("PhantomBot Core 1.5.7 6/20/2015");
+        com.gmt2001.Console.out.println("PhantomBot Core 1.5.8 6/30/2015");
         com.gmt2001.Console.out.println("Creator: mast3rplan");
         com.gmt2001.Console.out.println("Developers: gmt2001, GloriousEggroll, PhantomIndex");
         com.gmt2001.Console.out.println("www.phantombot.net");
