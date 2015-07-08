6cc6e0e5f41062adb18bd9651eadc937bd7d1a6c
GloriousEggroll <gloriouseggroll@gmail.com>
2015-07-08 19:32:04 -0400

    fix for bankheist start not working for moderators

--- res/scripts/systems/bankheistSystem.js
+++ res/scripts/systems/bankheistSystem.js
@@ -317,7 +317,7 @@ $.on('command', function(event) {
             }
         } else if(args[0].equalsIgnoreCase("start")) {
             
-            if(!$.isAdmin(sender) || !$.isMod(sender)){
+            if(!$.isMod(sender)){
                 $.say("You must be a moderator to use this command.");
                 return;
             }          
