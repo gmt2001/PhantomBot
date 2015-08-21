9a69aac14d1cc48bd1d228a4da0c5df5bcc458bc
Alex De Arnas <thephantomindex@gmail.com>
2015-08-21 13:17:16 -0400

    Fixed number of random messages showing on !random

--- res/scripts/commands/randomCommand.js
+++ res/scripts/commands/randomCommand.js
@@ -1,4 +1,4 @@
-﻿$.on('command', function(event) {
+$.on('command', function(event) {
     var sender = event.getSender();
     var command = event.getCommand();
     var num2 = $.users.length;    
@@ -7,7 +7,7 @@
     var argsString = event.getArguments().trim();
     var argsString2 = argsString.substring(argsString.indexOf(" ") + 1, argsString.length());
     var args = event.getArgs();
-    var num_randoms = parseInt($.inidb.get("randoms", "num_randoms"));
+    var num_randoms = parseInt($.inidb.get("random_num", "num_randoms"));
     var randomNum = $.randRange(1, 100);
     var num;
     
@@ -51,7 +51,7 @@
         }
         
       
-        $.inidb.incr("randoms", "num_randoms", 1);
+        $.inidb.incr("random_num", "num_randoms", 1);
         $.inidb.set("randoms", "random_" + num_randoms, argsString);
 
         
@@ -110,7 +110,7 @@
 
         $.inidb.del('randoms', 'random_' + (num_randoms - 1));
         
-        $.inidb.decr("randoms", "num_randoms", 1);
+        $.inidb.decr("random_num", "num_randoms", 1);
         
         $.say("random removed! There are now " + (num_randoms - 1) + " randoms!");
     }
@@ -156,38 +156,37 @@
 		ar.push("(sender) was thrown into a pit of genetically modified super lions.");
 		ar.push("(sender) was thrown into a pit of explosive prinnies.");
         ar.push("I want you (sender).");
-        ar.push("/me threw (sender) into a pit of PJSalt!" );
+        ar.push("/me threw (sender) into a pit of PJSalt" );
         ar.push("Dango, dango, dango, dango, big (sender) family~\u266B");
-        ar.push("What are we gonna do on the bed (sender)?");
+        ar.push("What are we gonna' do on the bed (sender)?");
         ar.push("Your mother was a hamster and your father smelt of elderberries!");
         ar.push("When life gives you lemons (sender), make orange juice and leave the world wondering how the hell you did it.");
         ar.push("(sender) was violated by a tentacle monster on stream.");
         ar.push("/me licked (sender). . .");
-        ar.push("This is the first time (sender) has noticed the scent of a woman.");
+        ar.push("This is the first time (sender) has noticed the scent of a woman..");
         ar.push("/me licking intensifies!");
         ar.push("I hope (sender)-senpai notices me..");
-        ar.push("(sender) is so awesome because (sender) can trip over flat surfaces, fall up the stair and also fall up!!");
+        ar.push("(sender) is so awesome because (sender) can trip over flat surfaces, fall up the stairs!!");
         ar.push("(sender) looks good enough to dress like the grim reaper and go to a retirement home and tap on the windows!");
         ar.push("Ok everyone shhhhh. The rice crispies are telling me what to do next.");
         ar.push("(sender) made eye contact with (random) while eating a banana!");
-        ar.push("It's weird, (sender) just doesn't have enough sax appeal.");
         ar.push("(random) mysteriously died.");
 		ar.push("(random) stopped existing.");
 		ar.push("All 7 of the Dragon Balls have been gathered! Now to wish (sender) to become a sloth!");
 		ar.push("The ocean is so salty because everyone pees in it.");
 		ar.push("Real men know when to run like a little bitch");
-		ar.push("If I get reincarnated…. I wanna become a clam.");
+		ar.push("If I get reincarnated…. I wanna be a clam.");
 		ar.push("Why don’t we drink to me and my reflection in your lovely eyes?");
 		ar.push("I can show you what color your brain is.");
 		ar.push("And while you’re waiting for us to kill you, we highly recommend p*ssing yourself, Followed by a course of praying to your impudent god. And cowarding in the corner and begging, always good. But if you act now, theres still time for an old-fashioned Suicide!! Thank you London! We love you! goodnight!!");
 		ar.push("If you die, don’t come crying to me about it.");
-		ar.push("From this point on, all you opinions will be rejected!");
+		ar.push("From this point on, all your opinions will be rejected!");
 		ar.push("Kakarot! Is That A Vegetable?! I Hate Vegetables.");
 		ar.push("I see. (sender) is the type of person who is thinking positively to the extent of being stupid.");
 		ar.push("I know as much of games as hugs and puppies, and care for them even less.");
 		ar.push("I’m not just a pervert…I’M A SUPER PERVERT");
-		ar.push("When you meet with scary people, you must always protect your wallet and asshole!");
-		ar.push("Women’s minds and Autumn Winds Change Often.");
+		ar.push("When you meet with scary people, you must always protect your wallet and anus!");
+		ar.push("Women’s minds and Autumn Winds change often.");
 		ar.push("I’m literally haemorrhaging generosity.");
 		ar.push("They were traps? I thought they were attractions.");
 		ar.push("Okay, here’s the plan: We go in, start hitting people, and see where it takes us.");
@@ -207,122 +206,20 @@
 		ar.push("I came to laugh at you - Chars Aznabal (Gundam)");
 		ar.push("You're thinking in Japanese! If you must think, do it in German!");
 		ar.push("*bursts into tears* I can't talk about it! It's so horrible! They were-- they were-- the milk! Oh God, the milk!");
-		ar.push("Man fears the darkness, and so he scrapes away at the edges of it with fire." );
 		ar.push("Panties, Panties, Panties");
 		ar.push("Now I understand the relieved, I feel very situation...");
-		ar.push("Nothing amazing happens here" );
 		ar.push("Come on, scribbles on the bathroom wall, please show me the path that I must follow!");
 		ar.push("Please don't worry, Mister, I only came here to obliterate you- not rob you.");
 		ar.push("... if you let the fly live, the spider is going to die. You can't save both without one suffering..");
 		ar.push("Bansai, bathtoy, wise guy, waterboy, lights shine bright in the o-town tonight" );
-		ar.push("Why don't we drink to me and my reflection in your lovely eyes?");
 		ar.push("Faye faye smoke smoke, faye faye puff puff! Yay!");
 		ar.push("Total slaughter, total slaughter. I won't leave a single man alive. La dee da dee dide, genocide. La dee da dee dud, an ocean of blood. Let's begin the killing time.");
 		ar.push("Someone once told me...that watching the birds made them want to go on a journey...");
 		ar.push("I wouldn't mind if you want to kill me, but I might struggle a bit");
 		ar.push("I think I know, I don't think I know, I don't think I think I know, I don't think I think.");
-		ar.push("To write with a broken pencil is pointless.");
-		ar.push("A bicycle can't stand on its own because it is two-tired.");
-		ar.push("Those who get too big for their britches will be exposed in the end.");
-		ar.push("When a clock is hungry it goes back four seconds.");
-		ar.push("A chicken crossing the road is poultry in motion.");
-		ar.push("If you don't pay your exorcist you get repossessed.");
-		ar.push("What's the definition of a will? It's a dead giveaway.");
-		ar.push("The man who fell into an upholstery machine is fully recovered.");
-		ar.push("Every calendar's days are numbered.");
-		ar.push("Bakers trade bread recipes on a knead to know basis.");
-		ar.push("When the electricity went off during a storm at a school the students were de-lighted.");
-		ar.push("I used to be a tap dancer until I fell in the sink.");
-		ar.push("He wears glasses during math because it improves division.");
-		ar.push("She was only a whisky maker but he loved her still.");
-		ar.push("She had a boyfriend with a wooden leg, but broke it off.");
-		ar.push("Those who jump off a Paris bridge are in Seine.");
-		ar.push("It wasn't school John disliked it was just the principal of it.");
-		ar.push("It's better to love a short girl than not a tall.");
-		ar.push("There was once a cross-eyed teacher who couldn't control his  pupils.");
-		ar.push("A grenade thrown into a kitchen in France would result in Linoleum Blownapart.");
-		ar.push("A boiled egg in the morning is hard to beat.");
-		ar.push("The one who invented the door knocker got a No-bell prize.");
-		ar.push("Old power plant workers never die they just de-generate.");
-		ar.push("There was a ghost at the hotel, so they called for an inn spectre.");
-		ar.push("With her marriage she got a new name and a dress.");
-		ar.push("The short fortune-teller who escaped from prison was a small medium at large");
-		ar.push("Some Spanish government employees are Seville servants.");
-		ar.push("He drove his expensive car into a tree and found out how the Mercedes bends.");
-		ar.push("Show me someone in denial and I'll show you a person in Egypt up to their ankles.");
-		ar.push("Two peanuts were walking in a tough neighbourhood and one of them was a-salted.");
-		ar.push("When cannibals ate a missionary they got a taste of religion.");
-		ar.push("When an actress saw her first strands of gray hair she thought she'd dye.");
-		ar.push("He often broke into song because he couldn't find the key.");
-		ar.push("Marathon runners with bad footwear suffer the agony of defeat.");
-		ar.push("Driving on so many turnpikes was taking its toll.");
-		ar.push("To some - marriage is a word ... to others - a sentence.");
-		ar.push("Old lawyers never die they just lose their appeal.");
-		ar.push("In democracy its your vote that counts. In feudalism its your count that votes.");
-		ar.push("Atheism is a non-prophet organization");
-		ar.push("It was an emotional wedding. Even the cake was in tiers.");
-		ar.push("Old skiers never die -- they just go down hill.");
-		ar.push("A cardboard belt would be a waist of paper.");
-		ar.push("Local Area Network in Australia: the LAN down under.");
-		ar.push("When the TV repairman got married the reception was excellent.");
-		ar.push("An office with many people and few electrical outlets could be in for a power struggle.");
-		ar.push("How do you make antifreeze? Steal her blanket.");
-		ar.push("A small boy swallowed some coins and was taken to a hospital. When his grandmother telephoned to ask how he was a nurse said 'No change yet'.");
-		ar.push("A pediatrician is a doctor of little patients.");
-		ar.push("Nylons give women a run for their money.");
-		ar.push("Talking to her about computer hardware I make my mother board.");
-		ar.push("Ancient orators tended to Babylon.");
-		ar.push("The best way to stop a charging bull is to take away his credit card.");
-		ar.push("If you give some managers an inch they think they're a ruler.");
-		ar.push("Two silk worms had a race. They ended up in a tie.");
-		ar.push("He had a photographic memory that was never developed.");
-		ar.push("Old burglars never die they just steal away.");
-		ar.push("A lawyer for a church did some cross-examining.");
-		ar.push("Chronic illegal parkers suffer from parking zones disease.");
-		ar.push("Some people don't like food going to waist..");
-		ar.push("A cannibal's favourite game is 'swallow the leader'.");
-		ar.push("You feel stuck with your debt if you can't budge it.");
-		ar.push("Girls who don't get asked out as often as their friends could feel out-dated.");
-		ar.push("We were so poor when I was growing up we couldn't even afford to pay attention. ");
-		ar.push("A pet store had a bird contest with no perches necessary.");
-		ar.push("A backwards poet writes inverse.");
-		ar.push("If a lawyer can be disbarred can a musician be denoted or a model deposed?");
-		ar.push("Once you've seen one shopping center you've seen a mall.");
-		ar.push("When the smog lifts in Los Angeles, U C L A.");
-		ar.push("A plateau is a high form of flattery.");
-		ar.push("When chemists die, we barium.");
-		ar.push("A long knife has been invented that cuts four loaves of bread at a time called a four loaf cleaver.");
-		ar.push("When the wheel was invented, it caused a revolution.");
-		ar.push("Two robbers with clubs went golfing, but they didn't play the fairway.");
-		ar.push("Seven days without a pun makes one weak.");
-		ar.push("A circus lion won't eat clowns because they taste funny.");
-		ar.push("A toothless termite walked into a tavern and said, \"Is the bar tender here?\"");
-		ar.push("Did you hear about the fire at the circus? The heat was intense.");
-		ar.push("A tattoo artist has designs on his clients.");
-		ar.push("Santa's helpers are subordinate clauses.");
-		ar.push("A lot of money is tainted. It taint yours and it taint mine.");
-		ar.push("When they bought a water bed, the couple started to drift apart.");
-		ar.push("What you seize is what you get.");
-		ar.push("Gardeners always know the ground rules.");
-		ar.push("Some people's noses and feet are build backwards: their feet smell and their noses run.");
-		ar.push("Two banks with different rates have a conflict of interest.");
-		ar.push("A successful diet is the triumph of mind over platter.");
-		ar.push("What do you call cheese that is not yours? Nacho Cheese.");
-		ar.push("When a new hive is done bees have a house swarming party.");
-		ar.push("Looting a drugstore is called Pillaging");
-		ar.push("Never lie to an x-ray technician. They can see right through you.");
-		ar.push("Old programmers never die, they just can't C as well.");
-		ar.push("A music store had a small sign which read: Bach in a Minuet.");
-		ar.push("Long fairy tales have a tendency to dragon.");
-		ar.push("Visitors to Cuba are usually Havana good time.");
-		ar.push("A bachelor is a guy who is footloose and fianc�e-free.");
-		ar.push("A ditch digger was entrenched in his career.");
-		ar.push("A girl and her boyfriend went to a party as a barcode.");
-		ar.push("They were an item.A criminal's best asset is his lie ability.");
 		ar.push("I love to stand in line at ATM machines, and when people put in their PIN, I yell GOT IT then run away ");
-		ar.push("Like a weird neighbor, stalkers are there!");
+		ar.push("Like a weird neighbour, stalkers are there!");
 		ar.push("OK think of a number. Add 12 to the number. Subtract 2. Divide that number by 5. Add 20. Did you get 12? Neither did I. I just wanted to see if you would do it! ");
-		ar.push("I'm going to get a job at walmart as a greeter and my words of welcome will be \"Welcome to freaking walmart! Get ur sh*t and get the hell out!!\" ");
 		ar.push("If somebody throws skittles at me and yells \"TASTE THE RAINBOW\", I'm gonna throw a 2 liter bottle of Dr.Pepper and yell \"TRUST ME I'M THE DOCTOR.\" ");
 		ar.push("A few days ago I very sternly told the voices in my head to stop talking to Me. Now they are sending me txt mgs say that they r sorry and want to get back2gethr.");
 		ar.push("Things to do at Walmart: hide behind teddy bears and make evil laughing noises when little kids come by.");
@@ -377,7 +274,6 @@
 		ar.push("I was in Tesco's and I saw this man and woman wrapped in a barcode. I said, ''Are you two an item?''   ");
 		ar.push("I'm in great mood tonight because the other day I entered a competition and I won a years supply of Marmite......... one jar.            ");
 		ar.push("So I went to the Chinese restaurant and this duck came up to me with a red rose and says ''Your eyes sparkle like diamonds''. I said, ''Waiter, I asked for a-ROMATIC duck''.");
-		ar.push("Four fonts walk into a bar the barman says ''Oi - get out! We don't want your type in here''        ");
 		ar.push("I was having dinner with Garry Kasporov (world chess champion) and there was a check tablecloth. It took him two hours to pass me the salt.");
 		ar.push("There was a man who entered a local paper's pun contest.. He sent in ten different puns, in the hope that at least one of the puns would win. Unfortunately, no pun in ten did.");
 		ar.push("I went down the local supermarket, I said, ''I want to make a complaint, this vinegar's got lumps in it'', he said, \"Those are pickled onions\".            ");
@@ -477,16 +373,15 @@
 		ar.push("Why are a wise man and a wise guy opposites?");
 		ar.push("Why do overlook and oversee mean opposite things?");
 		ar.push("If horrific means to make horrible, does terrific mean to make terrible?");
-		ar.push("Why isn't 11 pronounced onety one?");
 		ar.push("\"I am.\" is reportedly the shortest sentence in the English language. Could it be that \"I Do.\" is the longest sentence?");
 		ar.push("If lawyers are disbarred and clergymen defrocked, doesn't it follow that electricians can be delighted, musicians denoted, cowboys deranged, models deposed, tree surgeons debarked and drycleaners depressed?");
 		ar.push("Do Roman paramedics refer to IV's as \"4's\"?");
 		ar.push("Why is it that if someone tells you that there are 1 billion stars in the universe you will believe them, but if they tell you a wall has wet paint you will have to touch it to be sure?");
 		ar.push("If people from Poland are called \"Poles,\" why aren't people from Holland called 'Holes?");
 
-    if ($.inidb.get("randoms", "num_randoms") == null || $.inidb.get("randoms", "num_randoms") == 0 ) {
+    if ($.inidb.get("random_num", "num_randoms") == null || $.inidb.get("random_num", "num_randoms") == 0 ) {
         
-		$.inidb.set("randoms", "num_randoms", ar.length);
+		$.inidb.set("random_num", "num_randoms", ar.length);
                 for (var i=0; i< ar.length; ++i) {
                     $.inidb.set('randoms', 'random_' + i, ar[i]);
                 }
