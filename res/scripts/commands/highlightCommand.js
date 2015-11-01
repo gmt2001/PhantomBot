304dda0ddef34760403bda5d05432d7557e54fbb
scania_the_best <brandenbrideau1@gmail.com>
2015-11-01 17:49:09 -0400

    Update highlightCommand.js
    added lang support

--- res/scripts/commands/highlightCommand.js
+++ res/scripts/commands/highlightCommand.js
@@ -1,7 +1,7 @@
-var seconds = 0; // stream uptime in seconds
+var seconds = 0; 
 $.timer.addTimer("./commands/highlightCommand.js", "highlightcommand", true, function() {
     if (!$.isOnline($.channelName)) {
-        seconds=0;
+        seconds = 0;
         return;
     }
     else {
@@ -15,42 +15,37 @@ $.on('command', function (event) {
     var sender = event.getSender();
     var command = event.getCommand();
     var argsString = event.getArguments().trim();
-
 		
     if (command.equalsIgnoreCase("highlight")) {
         if (!$.isModv3(sender, event.getTags())) {
             $.say($.getWhisperString(sender) + $.modmsg);
         } else if (argsString.isEmpty()) { 
-            $.say($.getWhisperString(sender) + "Usage: !highlight (note)");
+            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.highlightcommand.usage"));
         } else if (!$.isOnline($.channelName)) {
-            $.say($.getWhisperString(sender) + "Stream is Offline!");
+            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.highlightcommand.error-stream-offline"));
         } else {
             var timestamp = $.getHighlight($.channelName);
             $.inidb.set("highlights", timestamp, argsString);
-            $.say($.getWhisperString(sender) + "Highlight saved! \"" + 
-                argsString + "\" @ " + timestamp + ".");
+            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.highlightcommand.highlight-saved", argsString, timestamp));
+            return;
         }
-        return;
     }
 	
     if (command.equalsIgnoreCase("clearhighlights")) {
-		
         if (!$.isModv3(sender, event.getTags())) {
             $.say($.getWhisperString(sender) + $.modmsg);
             return;
         }
 		
-        // TODO: Delete Highlights
         $.inidb.RemoveFile("highlights");
         $.inidb.ReloadFile("highlights");
-        $.say($.getWhisperString(sender) + "All Highlights have been erased!");
+        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.highlightcommand.highlight-cleared"));
+        return;
     }
-	
-    return;
 });
 setTimeout(function(){ 
     if ($.moduleEnabled('./commands/highlightCommand.js')) {
         $.registerChatCommand("./commands/highlightCommand.js", "highlight", "mod");
         $.registerChatCommand("./commands/highlightCommand.js", "clearhighlights", "mod");
     }
-}, 10*1000);
+}, 10 * 1000);
