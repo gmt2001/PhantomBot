22f558791d9ad7d93e2b91af06f57c758c2bd1f0
kojitsari <kojitsari@gmail.com>
2015-11-01 02:14:10 -0600

    Update 8ballCommand.js
    Pruned 8ball responses to be more logical (roughly 25% no, 25% grey-area, 50% yes), whipermode support for 8ball, and lang conversion.

--- res/scripts/commands/8ballCommand.js
+++ res/scripts/commands/8ballCommand.js
@@ -39,14 +39,36 @@ var points = $.inidb.get('points', sender);
             }
 
             if (args.length == 0 || args.length == null) { 
-		        $.say (username + ", ask the magic-8ball a question with !8ball"); 
+		        $.say ($.getWhisperString(sender) + $.lang.get("net.phantombot.8ballCommand.proper-usage")); 
 		        return; 
 		    }
 	
 	var ball = new Array();
 
-    ball.push("It is certain.");
-	ball.push("It is decidedly so.");
+	ball.push("Reply hazy try again.");
+	ball.push("Ask again later.");
+	ball.push("Better not tell you now.");
+	ball.push("Cannot predict now.");
+	ball.push("Outlook not so good.");
+	ball.push("Yes, in due time.");
+	ball.push("You will have to wait.");
+	ball.push("Ask again later.");
+	ball.push("Better not tell you now. OpieOP");
+	ball.push("Concentrate and ask again.");
+	ball.push("Reply hazy, try again.");
+	ball.push("Never going to happen!");
+	ball.push("The odds of that happening are pretty slim.");
+	ball.push("My reply is no.");
+	ball.push("My sources say no.");
+	ball.push("Very doubtful.");
+	ball.push("No.");
+	ball.push("I have no response for that question...");
+	ball.push("Why would I tell you? OMGScoots");	
+	ball.push("Forget about it.");
+	ball.push("Don't bet on it.");
+	ball.push("Who knows?");
+	ball.push("Signs point to yes.");
+	ball.push("It is certain.");
 	ball.push("Without a doubt.");
 	ball.push("Yes definitely.");
 	ball.push("You may rely on it.");
@@ -55,97 +77,41 @@ var points = $.inidb.get('points', sender);
 	ball.push("Outlook good.");
 	ball.push("Yes.");
 	ball.push("Signs point to yes.");
-	ball.push("Reply hazy try again.");
-	ball.push("Ask again later.");
-	ball.push("Better not tell you now.");
-	ball.push("Cannot predict now.");
-	ball.push("Concentrate and ask again.");
-	ball.push("Don't count on it.");
-	ball.push("My reply is no.");
-	ball.push("My sources say no.");
-	ball.push("Outlook not so good.");
-	ball.push("Very doubtful.");
-	ball.push("Yes, in due time.");
-	ball.push("You will have to wait.");
-	ball.push("Ask again later.");
-	ball.push("Better not tell you now. OpieOP");
-	ball.push("Concentrate and ask again.");
-	ball.push("Reply hazy, try again.");
-	ball.push("What do you think?");
-	ball.push("Never going to happen!");
-	ball.push("The odds of that happening are pretty slim.");
-	ball.push("The end of the world as we know it shall occur before that happens...");
-	ball.push("No, just no DansGame");
-	ball.push("You cannot ask me that without buying me dinner first!");
-	ball.push("404: response not found.");
-	ball.push("Oh dear I appear to be broken!");
-	ball.push("My magic fluid is in need of a change!");
-	ball.push("凸( •̀_•́ )凸");
-	ball.push("The odds are over 9000!");
-	ball.push("I believe this answers your question? ᕕ╏ ͡ᵔ ‸ ͡ᵔ ╏凸");
-	ball.push("You're too young to hear my response Kappa !");
-	ball.push("I'm on my union break!");
-	ball.push("Where is my incentive to answer?");
-	ball.push("I have no response for that question...");
-	ball.push("I can't believe you asked that FailFish");
-	ball.push("May the odds be forever in your favour!");
-	ball.push("Please OpieOP");
-	ball.push("Don't ask me that DansGame");
-	ball.push("Nah.");
-	ball.push("Why would you ask me that? OMGScoots");
-	ball.push("No.");
-	ball.push("Why would I tell you? OMGScoots");
-	ball.push("Forget about it.");
-	ball.push("Don't bet on it.");
-	ball.push("Go for it!");
-	ball.push("Probably.");
-	ball.push("Who knows?");
-	ball.push("It is certain.");
-	ball.push("Signs point to yes.");
-	ball.push("As I see it, yes.");
-	ball.push("Master gave me a sock! I'M FREEEEEEEEEEE!");
-	ball.push("/me Accio Answer!");
-	ball.push("/me gotta find the answer....gotta find the answer....shit they're on to me! WutFace ");
-	ball.push("You need a higher clearance to access this information!");
-	ball.push("You're on the blacklist aren't you?");
-	ball.push("You failed the intelligence check for an answer");
-	ball.push("I'd give you an answer, but I took an arrow to the knee! Or was it the head OMGScoots ?");
 	ball.push("This is not the Bot you're looking for ༼ﾉ۞⌂۞༽ﾉ");
-	ball.push("Do you like to work on your lunch break?! Because I won't /╲/\╭༼ಥДಥ༽╮/\╱﻿\ ");
-	ball.push("Doot doot, do do doot! Your Pokémon are healed and ready to go! Oh wait....");
+	
 	
 	do {
 		b = $.randElement(ball);
 	} while (b.equalsIgnoreCase($var.lastRandom) && ball.length > 1);
 	if (points < ballcost) {
-		$.say("You need more points to use this command " + username);
+		$.say($.getWhisperString(sender) + $.lang.get("net.phantombot.8ballCommand.not-enough-points"));
 		return;
 	} else {
-	    $.say("/me @"+ username +", "+ b);
+	    $.say($.getWhisperString(sender) + b);
             return;
     }
 }
 
 	if (command.equalsIgnoreCase("8ballcooldown")) {
         if (!$.isAdmin(sender)) {
-            $.say($.adminmsg);
+            $.say($.getWhisperString(sender) + $.adminmsg);
             return;
     }
 
         $.inidb.set('settings', 'ballcooldown', args[0]);
         $.ballcooldown = $.inidb.get('settings', 'ballcooldown');
-        $.say(username +", new cooldown set!");
+        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.8ballCommand.new-cooldown"));
     }
 
     if (command.equalsIgnoreCase("8ballcost")) {
         if (!$.isAdmin(sender)) {
-            $.say($.adminmsg);
+            $.say($.getWhisperString(sender)+ $.adminmsg);
             return;
     }
 
         $.inidb.set('settings', 'ballcost', args[0]);
         $.ballcost = $.inidb.get('settings', 'ballcost');
-        $.say(username +", new cost set!");
+        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.8ballCommand.new-cost"));
     }
 });
 setTimeout(function(){ 
