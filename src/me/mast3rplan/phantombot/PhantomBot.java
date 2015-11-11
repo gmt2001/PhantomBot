9cda5f1d06225724127640c9ea49e87573e3750d
gmt2001 <garymt2001@hotmail.com>
2015-11-11 03:32:40 -0600

    Updated build date for release

--- src/me/mast3rplan/phantombot/PhantomBot.java
+++ src/me/mast3rplan/phantombot/PhantomBot.java
@@ -121,7 +121,7 @@ public class PhantomBot implements Listener
         Thread.setDefaultUncaughtExceptionHandler(com.gmt2001.UncaughtExceptionHandler.instance());
 
         com.gmt2001.Console.out.println();
-        com.gmt2001.Console.out.println("PhantomBot Core 1.6.5 10/25/2015");
+        com.gmt2001.Console.out.println("PhantomBot Core 1.6.5 11/11/2015");
         com.gmt2001.Console.err.println("Build revision " + RepoVersion.getRepoVersion());
         com.gmt2001.Console.out.println("Creator: mast3rplan");
         com.gmt2001.Console.out.println("Developers: gmt2001, GloriousEggroll, PhantomIndex");
