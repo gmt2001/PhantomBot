860c8da2aae3a6306753005e11a45cd9addf4b20
gmt2001 <garymt2001@hotmail.com>
2015-11-26 23:06:44 -0600

    Update donationHandler.js for multi-channel support

--- res/scripts/handlers/donationHandler.js
+++ res/scripts/handlers/donationHandler.js
@@ -1,94 +1,80 @@
-$.checkerstorepath = $.inidb.get('settings','checker_storepath');
-if ($.checkerstorepath == null || $.checkerstorepath == "" || $.strlen($.checkerstorepath) == 0) {
-    $.checkerstorepath = "addons/donationchecker/latestdonation.txt";
-}
+$.on('ircJoinComplete', function (event) {
+    if (!$.inidb.Exists('settings', event.getChannel().getName(), 'checker_storepath')) {
+        $.inidb.SetString('settings', event.getChannel().getName(), 'checker_storepath', "addons/donationchecker/latestdonation.txt");
+    }
 
-$.donation_toggle = $.inidb.get('settings','donation_toggle');
-if ($.donation_toggle == null || $.donation_toggle == "" || $.strlen($.donation_toggle) == 0) {
-    $.donation_toggle = 1;
-}
+    if (!$.inidb.Exists('settings', event.getChannel().getName(), 'donation_toggle')) {
+        $.inidb.SetBoolean('settings', event.getChannel().getName(), 'checker_storepath', true);
+    }
+});
 
 $.on('command', function (event) {
     var sender = event.getSender();
     var command = event.getCommand();
     var args = event.getArgs();
-    var argsString = event.getArguments().trim();
-    
-    if (argsString.isEmpty()) {
-        args = [];
-    } else {
-        args = argsString.split(" ");
-    }
-    
+    var channel = event.getChannel();
+
     if (command.equalsIgnoreCase("donationalert")) {
-        if (!$.isAdmin(sender)) { // added this check so people can't spam the usage.
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.adminonly"));
+        if (!$.isAdmin(sender, channel)) { // added this check so people can't spam the usage.
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.cmd.adminonly", channel), channel);
             return;
-        } 
+        }
+
+        if (args.length == 0) { // added usage
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.donationhandler.donationalert-usage", channel), channel);
+            return;
+        }
 
         var action = args[0];
 
-        if (args.length == 0) { // added usage
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.donationhandler.donationalert-usage"));
-            return;
-        }
-        
         if (action.equalsIgnoreCase("filepath")) {
-            if (!$.isAdmin(sender)) {
-                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.adminonly"));
+            if (!$.isAdmin(sender, channel)) {
+                $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.cmd.adminonly", channel), channel);
                 return;
             }
-            
+
             if (args[1].equalsIgnoreCase('viewfilepath')) {
-                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.donationhandler.current-file-path", $.checkerstorepath));
+                $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.donationhandler.current-file-path", channel, $.inidb.GetString('settings', channel.getName(), 'checker_storepath')), channel);
                 return;
             }
-            
-            while (args[1].indexOf('\\') != -1 && !args[1].equalsIgnoreCase('viewfilepath') && args[1] != "" && args[1] != null) {
+
+            while (args[1].indexOf('\\') != -1 && !args[1].equalsIgnoreCase('viewfilepath') && $.strlen(args[1]) > 0) {
                 args[1] = args[1].replace('\\', '/');
             }
-            
-            $.inidb.set('settings','checker_storepath', args[1]);
-            $.checkerstorepath = args[1];
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.donationhandler.new-file-path-set"));
+
+            $.inidb.SetString('settings', channel.getName(), 'checker_storepath', args[1]);
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.donationhandler.new-file-path-set", channel), channel);
             return;
         }
 
         if (action.equalsIgnoreCase("toggle")) {
-            if ($.donation_toggle == 1) {
-                $.inidb.set('settings','donation_toggle', 0);
-                $.donation_toggle = 0;
-                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.donationhandler.donation-toggle-off"));
+            if ($.inidb.GetBoolean('settings', channel.getName(), 'donation_toggle')) {
+                $.inidb.SetBoolean('settings', channel.getName(), 'donation_toggle', false);
+                $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.donationhandler.donation-toggle-off", channel), channel);
                 return;
             } else {
-                $.inidb.set('settings','donation_toggle', 1);
-                $.donation_toggle = 1;
-                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.donationhandler.donation-toggle-on"));
+                $.inidb.SetBoolean('settings', channel.getName(), 'donation_toggle', true);
+                $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.donationhandler.donation-toggle-on", channel), channel);
                 return;
             }
         }
     }
 });
 
-//Q: why is there a timeout delay here before a timer? seems redundant no?
-//A: the timeout sets a delay to start the timer, otherwise the timer won't detect if a module is disabled (because it hasnt loaded in yet)
-setTimeout(function(){ 
-    if ($.moduleEnabled("./handlers/donationHandler.js")) {
-        $.timer.addTimer("./handlers/donationHandler.js", "currdonation", true, function() {
-            $var.currDonation = $.readFile($.checkerstorepath);
-            if ($var.currDonation.toString() != $.inidb.get("settings", "lastdonation")) {
-                if ($var.currDonation.toString() != null || $var.currDonation.toString() != "") {
-                    $.inidb.set("settings", "lastdonation", $.readFile($.checkerstorepath));
-                    if ($.donation_toggle == 1) {
-                        $.say($.username.resolve($.ownerName) + $.lang.get("net.phantombot.donationhandler.new-donation", $.readFile($.checkerstorepath)));
-                        return;
-                    } else if ($.donation_toggle == 0) {
-                        $.say($.username.resolve($.ownerName) + $.lang.get("net.phantombot.donationhandler.new-donation", $.readFile($.checkerstorepath)));
-                        return;
-                    }
-                }
+$.timer.addTimer("./handlers/donationHandler.js", "currdonation", true, function () {
+    var channels = $.phantombot.getChannels();
+
+    for (var i = 0; i < channels.size(); i++) {
+        var channel = channels.get(i);
+
+        if ($.moduleEnabled("./handlers/donationHandler.js", channel) && $.inidb.GetBoolean('settings', channel.getName(), 'donation_toggle')) {
+            var currDonation = $.readFile($.inidb.GetString('settings', channel.getName(), 'checker_storepath'));
+            if (currDonation != null && $.strlen(currDonation) > 0 && !currDonation.equalsIgnoreCase($.inidb.GetString("settings", channel.getName(), "lastdonation"))) {
+                $.inidb.SetString("settings", channel.getName(), "lastdonation", currDonation);
+                $.say($.lang.get("net.phantombot.donationhandler.new-donation", channel, $.username.resolve(channel.getName().replaceFirst("#", "")), currDonation), channel);
             }
-        }, 10 * 1000);
+        }
     }
-    $.registerChatCommand("./handlers/donationHandler.js", "donationalert", "mod");
 }, 10 * 1000);
+
+$.registerChatCommand("./handlers/donationHandler.js", "donationalert", "mod");
