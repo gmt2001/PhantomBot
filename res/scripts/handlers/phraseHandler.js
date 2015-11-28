09c3cf0d6253400f49b58ad6c01608645f88df5e
gmt2001 <garymt2001@hotmail.com>
2015-11-28 01:57:05 -0600

    Updated phraseHandler.js for multi-channel support

--- res/scripts/handlers/phraseHandler.js
+++ res/scripts/handlers/phraseHandler.js
@@ -1,79 +1,65 @@
-$.on('ircChannelMessage', function(event) { 
-    var message = new String(event.getMessage().toLowerCase().trim());
+$.on('ircChannelMessage', function (event) {
+    var message = event.getMessage().toLowerCase().trim();
     var sender = event.getSender();
     var username = $.username.resolve(sender, event.getTags());
+    var channel = event.getChannel();
 
-    message = message.replace(/[^a-zA-Z0-9\s]+/g,'');
-    var emoteKey = $.inidb.GetKeyList("phrases", "");
-    if (emoteKey == null || emoteKey[0] == "" || emoteKey[0] == null) {
-        return;
+    message = message.replace(/[^a-zA-Z0-9\s]+/g, '');
+
+    if ($.inidb.Exists('phrases', channel.getName(), message)) {
+        var s = $.inidb.GetString('phrases', channel.getName(), message);
+
+        s = $.replaceAll(s, "(sender)", username);
+
+        $.say(s, channel);
     }
-
-    for (i = 0; i < emoteKey.length; i++) {
-        if (message.equalsIgnoreCase(emoteKey[i].toLowerCase())) {
-            var messageKEY = $.inidb.get('phrases', emoteKey[i]);
-            
-            messageKEY = $.replaceAll(messageKEY, "(sender)", username);
-			
-            $.say(messageKEY);
-            return;
-        }
-    }    
 });
 
 $.on('command', function (event) {
     var sender = event.getSender();
     var command = event.getCommand();
     var argsString = event.getArguments().trim();
-    var argsString2 = argsString.substring(argsString.indexOf(" ") + 1, argsString.length());
     var args = event.getArgs();
-    var triggerphrase = "";
-    var response = "";
-	
+    var channel = event.getChannel();
+
     if (command.equalsIgnoreCase("addphrase")) {
-        if (!$.isMod(sender, event.getTags(), event.getChannel())) { 
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.modonly"));
+        if (!$.isMod(sender, event.getTags(), channel)) {
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.cmd.modonly", channel), channel);
             return;
         }
 
         if (args.length < 2) {
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.phrasehandler.trigger-error-add-usage"));
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.phrasehandler.trigger-error-add-usage", channel), channel);
             return;
-        } 
+        }
 
-        triggerphrase = args[0].toLowerCase();
-        triggerphrase = new String(triggerphrase);
-        triggerphrase = triggerphrase.replace(/[^a-zA-Z0-9\s]+/g,'');
-        
-        response = args[1];
-         
-        $.inidb.set('phrases', triggerphrase, response);
-        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.phrasehandler.trigger-add-success", triggerphrase, response));
+        argsString = argsString.substring(argsString.indexOf(args[0]) + $.strlen(args[0]) + 1);
+        args[0] = args[0].toLowerCase().replace(/[^a-zA-Z0-9\s]+/g, '');
+
+        $.inidb.SetString('phrases', channel.getName(), args[0], argsString);
+
+        $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.phrasehandler.trigger-add-success", channel, args[0], argsString), channel);
         return;
     }
-    
+
     if (command.equalsIgnoreCase("delphrase")) {
-        if (!$.isMod(sender, event.getTags(), event.getChannel())) { 
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.modonly"));
+        if (!$.isMod(sender, event.getTags(), channel)) {
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.cmd.modonly", channel), channel);
             return;
         }
 
-        if (args.length == 0) { 
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.phrasehandler.trigger-remove-usage"));
+        if (args.length == 0) {
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.phrasehandler.trigger-remove-usage", channel), channel);
             return;
         }
 
-        triggerphrase = args[0].toLowerCase();
-        
-        $.inidb.del('phrases', triggerphrase);
-        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.phrasehandler.trigger-remove-success", triggerphrase));
+        args[0] = args[0].toLowerCase().replace(/[^a-zA-Z0-9\s]+/g, '');
+
+        $.inidb.RemoveKey('phrases', channel.getName(), args[0]);
+        $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.phrasehandler.trigger-remove-success", channel, args[0]), channel);
         return;
     }
 });
 
-setTimeout(function(){ 
-    if ($.moduleEnabled('./handlers/phraseHandler.js')) {
-        $.registerChatCommand("./handlers/phraseHandler.js", "addphrase", "mod");
-        $.registerChatCommand("./handlers/phraseHandler.js", "delphrase", "mod");
-    }
-},10 * 1000);
+$.registerChatCommand("./handlers/phraseHandler.js", "addphrase", "mod");
+$.registerChatCommand("./handlers/phraseHandler.js", "delphrase", "mod");
