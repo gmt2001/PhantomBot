db6b58b64618be026ead334b324777bd983be8cd
gmt2001 <garymt2001@hotmail.com>
2015-11-28 18:07:15 -0600

    Update raidSystem.js for multi-channel support

--- res/scripts/systems/raidSystem.js
+++ res/scripts/systems/raidSystem.js
@@ -1,95 +1,88 @@
-var maxSpamCount = 10;
-
-$.getOrdinal = function (n) {
-    var p = $.lang.get("net.phantombot.common.ordinal-prefixes");
-    var s = $.lang.get("net.phantombot.common.ordinal-suffixes");
+$.getOrdinal = function (n, channel) {
+    var p = $.lang.get("net.phantombot.common.ordinal-prefixes", channel);
+    var s = $.lang.get("net.phantombot.common.ordinal-suffixes", channel);
     var v = n % 100;
 
     if (p.length == 10 && s.length == 10) {
         return (p[(v - 20) % 10] || p[v] || p[0]) + n + (s[(v - 20) % 10] || s[v] || s[0]);
     } else if (p.length != 10) {
-        $.logError("./systems/raidSystem.js", 6, "The ordinal prefixes did not contain all numbers. String: net.phantombot.common.ordinal-prefixes");
+        $.logError("./systems/raidSystem.js", 6, channel, "The ordinal prefixes did not contain all numbers. String: net.phantombot.common.ordinal-prefixes");
         println("[raidSystem.js] The ordinal prefixes did not contain all numbers. String: net.phantombot.common.ordinal-prefixes");
         return n;
     } else if (s.length != 10) {
-        $.logError("./systems/raidSystem.js", 7, "The ordinal suffixes did not contain all numbers. String: net.phantombot.common.ordinal-suffixes");
+        $.logError("./systems/raidSystem.js", 7, channel, "The ordinal suffixes did not contain all numbers. String: net.phantombot.common.ordinal-suffixes");
         println("[raidSystem.js] The ordinal suffixes did not contain all numbers. String: net.phantombot.common.ordinal-suffixes");
         return n;
     }
 }
 
-$.on('command', function(event) {
+$.on('command', function (event) {
     var sender = event.getSender();
     var username = $.username.resolve(sender, event.getTags());
     var command = event.getCommand();
     var argsString = event.getArguments().trim();
-    var args;
-
-    if (argsString.isEmpty()) {
-        args = [];
-    } else {
-        args = argsString.split(" ");
-    }
+    var args = event.getArgs();
+    var channel = event.getChannel();
 
     if (command.equalsIgnoreCase("raid")) {
         if (args.length >= 1) {
-            if (!$.isModv3(sender, event.getTags())) {
-                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.modonly"));
+            if (!$.isMod(sender, event.getTags(), channel)) {
+                $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.cmd.modonly", channel), channel);
                 return;
             }
-            
-            if (args[1] == null) {
-                $.say($.lang.get("net.phantombot.raidsystem.raid-success", args[0].toLowerCase(), ""));
+
+            if (args.length == 1) {
+                $.say($.lang.get("net.phantombot.raidsystem.raid-success", channel, $.username.resolve(args[0]), ""), channel);
                 return;
             }
 
             if (args.length >= 2) {
+                var count = 1;
+                argsString = argsString.substring(argsString.indexOf(args[0]) + $.strlen(args[0]) + 1);
+
                 if (!isNaN(parseInt(args[1]))) {
-                    if (parseInt(args[1]) > maxSpamCount) {
-                        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.raidsystem.raid-error-toomuch", maxSpamCount));
+                    if (parseInt(args[1]) > 10) {
+                        $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.raidsystem.raid-error-toomuch", channel, 10), channel);
                         return;
                     }
-                    
-                    for (var i = 0; i < parseInt(args[1]); i++) {
-                        if(args[2] != null) {
-                            $.say($.lang.get("net.phantombot.raidsystem.raid-success", args[0].toLowerCase(), argsString.substring(argsString.indexOf(args[2].toString()), argsString.length())));
-                        } else {
-                            $.say($.lang.get("net.phantombot.raidsystem.raid-success", args[0].toLowerCase(), ""));                                
-                        }
+
+                    count = parseInt(args[1]);
+
+                    if (args.length > 2) {
+                        argsString = argsString.substring(argsString.indexOf(args[0]) + $.strlen(args[0]) + $.strlen(args[1]) + 2);
+                    } else {
+                        argsString = "";
                     }
-                    return;
-                } else {
-                    $.say($.lang.get("net.phantombot.raidsystem.raid-success", args[0].toLowerCase(), argsString.substring(argsString.indexOf(args[1].toString()), argsString.length())));
-                    return;
                 }
+
+                for (var i = 0; i < count; i++) {
+                    $.say($.lang.get("net.phantombot.raidsystem.raid-success", channel, $.username.resolve(args[0]), argsString), channel);
+                }
+                return;
             }
         } else {
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.raidsystem.raid-usage"));       
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.raidsystem.raid-usage", channel), channel);
             return;
         }
     }
-    
+
     if (command.equalsIgnoreCase("raider")) {
-        if (args.length >= 1) {
-            if (!$.isModv3(sender, event.getTags())) {
-                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.modonly"));
+        if (args.length > 0) {
+            if (!$.isMod(sender, event.getTags(), channel)) {
+                $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.cmd.modonly", channel), channel);
                 return;
             }
 
-            $.inidb.incr('raiders', args[0].toLowerCase() + "_count", 1);
+            $.inidb.SetInteger('raiders', channel.getName(), args[0].toLowerCase() + "_count", $.inidb.GetInteger('raiders', channel.getName(), args[0].toLowerCase() + "_count") + 1);
 
-            $.say($.lang.get("net.phantombot.raidsystem.raider-success", $.username.resolve(args[0]), getOrdinal($.inidb.get('raiders', args[0].toLowerCase()  + "_count")), args[0].toLowerCase()));  
+            $.say($.lang.get("net.phantombot.raidsystem.raider-success", channel, $.username.resolve(args[0]), $.getOrdinal($.inidb.GetInteger('raiders', channel.getName(), args[0].toLowerCase() + "_count"), channel), $.username.resolve(args[0])), channel);
             return;
         } else {
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.raidsystem.raider-usage"));       
+            $.say($.getWhisperString(sender, channel) + $.lang.get("net.phantombot.raidsystem.raider-usage", channel), channel);
             return;
         }
     }
 });
 
-setTimeout(function(){ 
-    if ($.moduleEnabled('./systems/raidSystem.js')) {
-        $.registerChatCommand("./systems/raidSystem.js", "raid", "mod");
-        $.registerChatCommand("./systems/raidSystem.js", "raider", "mod");
-    }
-},10*1000);
+$.registerChatCommand("./systems/raidSystem.js", "raid", "mod");
+$.registerChatCommand("./systems/raidSystem.js", "raider", "mod");
