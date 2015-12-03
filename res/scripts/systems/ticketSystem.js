7d76f5fffd31229933397980785cd08970e43d62
Branden B <brandenbrideau1@gmail.com>
2015-12-03 18:35:49 -0400

    Update ticketSystem.js
    Added follower only.
    Added subscriber only.
    Added subscriber luck.
    Added total entries.
    Made sure that it was removing points once you bought tickets.

--- res/scripts/systems/ticketSystem.js
+++ res/scripts/systems/ticketSystem.js
@@ -10,28 +10,55 @@ $.on('command', function(event) {
     var args = event.getArgs();
    
     if (command.equalsIgnoreCase("tickets")) {
-        if (args.length == 0) {
-            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.usage", $.ticketCost));
-            return;
-        } else if ($.TicketRaffleRunning == false) {
+        if ($.TicketRaffleRunning == false) {
             $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketrafflesystem.no-raffle-opened"));
             return;
-        } else if ($.ticketCost > parseInt($.inidb.get("points", sender))) {
+        } else if (args.length == 0) {
+            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.usage", $.ticketCost));
+            return;
+        } else if ($.ticketCost > $.inidb.get("points", sender)) {
             $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.need-more-points"));
             return;
-        } else if ($.TicketRaffleMaxEntries < args[0]) {
+        } else if ($.TicketRaffleMaxEntries < parseInt(args[0])) {
             $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.max-tickets-allowed", $.TicketRaffleMaxEntries));
             return;
         } else if ($.inidb.get("traffleplayer", sender) == "true") {
             $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.error-already-in-raffle"));
             return;
         }
-
-        for (var i = 0; i < parseInt(args[0]); i++) {
-            $.TicketRaffleEntries.push(sender);
+        if ($.Followers == true) {
+            if ($.inidb.get("followed", sender) == null) {
+                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketrafflesystem.following"));
+                return;
+            }
+        } else if ($.Subscribers == true) {
+            if ($.inidb.get("subscribed", sender) == null) {
+                $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketrafflesystem.no-sub"));
+                return;
+            }
         }
-        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.buy-success", parseInt(args[0])));
+        var tickets = parseInt(args[0]);
+        if ($.inidb.get("subscribed", sender) == true) {
+            for (var i = 0; i < tickets * $.SubscriberLuck; i++) {
+                $.TicketRaffleEntries.push(sender);
+            }
+        } else {
+            for (var i = 0; i < tickets; i++) {
+                $.TicketRaffleEntries.push(sender);
+            }
+        }
+        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.buy-success", tickets));
         $.inidb.set("traffleplayer", sender, "true");
+        $.inidb.decr("points", sender, (tickets * $.ticketCost));
+        return;
+    }
+
+    if (command.equalsIgnoreCase("entries")) {
+        if (!$.isAdmin(sender)) {
+            $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.adminonly"));
+            return;
+        }
+        $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.total-entries", $.TicketRaffleEntries.length));
         return;
     }
 
@@ -40,6 +67,7 @@ $.on('command', function(event) {
             $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.cmd.adminonly"));
             return;
         }
+        
         if (args.length == 0) {
             $.say($.getWhisperString(sender) + $.lang.get("net.phantombot.ticketsystem.cost-usage"));
             return;
@@ -47,7 +75,7 @@ $.on('command', function(event) {
 
         if (args[0].equalsIgnoreCase("cost")) {
             $.inidb.set("settings", "ticketcost", parseInt(args[1]));
-            $.say($.lang.get("net.phantombot.ticketsystem.new-cost", parseInt(args[1]), $.pointname));
+            $.say($.lang.get("net.phantombot.ticketsystem.new-cost", parseInt(args[1]), "point(s)"));
             return;
         }
     }
